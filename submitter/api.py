2db982b3f7ae29a75e280975ed4d3f7848da5742
hmdcpc2021 <hamicpc2021@gmail.com>
2023-08-07 10:51:40 +0100

    API updated

--- submitter/api.py
+++ submitter/api.py
@@ -1,10 +1,17 @@
-from flask import Flask
 from werkzeug.exceptions import HTTPException
+from flask import Flask, jsonify
+import logging
+
 
 from submitter.apis.v1.api import v1blueprint
 from submitter.apis.v2.controller import v2blueprint
 
+from submitter.adaptors.k8s_adaptor import KubernetesAdaptor
+from submitter.adaptors.occopus_adaptor import OccopusAdaptor
 app = Flask(__name__)
+
+kubernetes_adaptor = KubernetesAdaptor()
+occopus_adaptor = OccopusAdaptor()
 
 app.register_blueprint(v1blueprint)
 app.register_blueprint(v2blueprint, url_prefix="/v2.0/")
@@ -26,3 +33,30 @@ def not_found_errors(error):
         {"message": str(error)},
         getattr(error, "code", 500),
     )
+
+@app.route('/v2.0/micado-info')
+def get_micado_info():
+    try:
+        ip_address, port_number = kubernetes_adaptor.info()
+        # Return the MiCADO info as JSON response
+        response = {
+            "ip_address": ip_address,
+            "port_number": port_number
+        }
+        return jsonify(response)
+    except Exception as e:
+        return (
+            {"message": f"Error fetching MiCADO info: {str(e)}"},
+            500,
+        )
+
+@app.route('/v2.0/info', methods=['GET'])
+def get_info():
+    """
+    Get the internal and external IPs from MiCADO for each node.
+    """
+    try:
+        ips_info = occopus_adaptor.info()
+        return jsonify(ips_info)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
